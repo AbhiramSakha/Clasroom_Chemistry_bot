250568eba5a0bab9cd4ad9dc67458f5686062f56
AbhiramSakha <143825001+AbhiramSakha@users.noreply.github.com>
2026-04-30 15:24:47 +0530

    Initial Commit

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb+srv://sakhabhiram1234_db_user:VuEf7lDZeIIw7iab@cluster0.ezt3bs1.mongodb.net/?appName=Cluster0")
 db = client["chem_ai"]
 
 users_col = db["users"]
